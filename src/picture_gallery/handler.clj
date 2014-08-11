c685d9f0e9d97e1f2752892fc3f46ab315196a7b
Sebastian Bensusan <sbensu@gmail.com>
2014-08-11 16:00:36 +0200

    Users Table Added

--- src/picture_gallery/handler.clj
+++ src/picture_gallery/handler.clj
@@ -1,11 +1,8 @@
 (ns picture-gallery.handler
   (:require [compojure.core :refer [defroutes routes]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.file-info :refer [wrap-file-info]]
-            [hiccup.middleware :refer [wrap-base-url]]
-            [compojure.handler :as handler]
             [compojure.route :as route]
-            [picture-gallery.routes.home :refer [home-routes]]))
+            [picture-gallery.routes.home :refer [home-routes]]
+            [noir.util.middleware :as noir-middleware]))
 
 (defn init []
   (println "picture-gallery is starting"))
@@ -18,8 +15,5 @@
   (route/not-found "Not Found"))
 
 (def app
-  (-> (routes home-routes app-routes)
-      (handler/site)
-      (wrap-base-url)))
-
+  (noir-middleware/app-handler [home-routes app-routes]))
 
---
+++ src/picture_gallery/models/db.clj
@@ -0,0 +1,9 @@
+(ns picture-gallery.models.db
+  (:require [clojure.java.jdbc :as sql]))
+
+(def db
+  {:subprotocol "postgresql"
+   :subname "//localhost/gallery"
+   :user "admin"
+   :password "admin"})
+
